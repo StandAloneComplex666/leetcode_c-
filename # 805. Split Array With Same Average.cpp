6f10bf081f3cc6efa254d8f5c79a4dd2919382ef
Stand Alone Complex <yc2739@nyu.edu>
2018-08-06 20:41:20 -0400

    no message

---
+++ # 805. Split Array With Same Average.cpp
@@ -0,0 +1,35 @@
+
+class Solution {
+public:
+    bool splitArraySameAverage(vector<int>& A) {
+        int n = A.size(), m = n / 2;
+        int totalSum = accumulate(A.begin(), A.end(), 0);
+        // early pruning
+        bool isPossible = false;
+        for (int i = 1; i <= m; ++i) {
+            if (totalSum * i % n == 0) {
+                isPossible = true;
+                break;
+            }
+        }  
+        if (!isPossible) {
+            return false;
+        }
+        // DP like knapsack
+        vector<unordered_set<int>> sums(m + 1);
+        sums[0].insert(0);
+        for (int num: A) {  // for each element in A, we try to add it to sums[i] by joining sums[i - 1]
+            for (int i = m; i >= 1; --i) {
+                for (const int t: sums[i - 1]) {
+                    sums[i].insert(t + num);
+                } 
+            }
+        }
+        for (int i = 1; i <= m; ++i) {
+            if (totalSum * i % n == 0 && sums[i].find(totalSum * i / n) != sums[i].end()) {
+                return true;
+            }
+        } 
+        return false;
+    }
+};
