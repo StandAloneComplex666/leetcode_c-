7dcb1fc63c6671bc17528a4172db003d815cf8e7
Stand Alone Complex <yc2739@nyu.edu>
2018-08-07 19:08:47 -0400

    no message

--- # 807. Max Increase to Keep City Skyline.py
+++ # 807. Max Increase to Keep City Skyline.py
@@ -20,3 +20,20 @@ class Solution:
             for j in range(len(grid[0])):
                res += min(maxTopBottom[i], maxLeftRight[j]) - grid[i][j]
         return res
+
+
+# others' version:
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        rows_max = [max(row) for row in grid]
+        columns = [[row[i] for row in grid] for i in range(len(grid))]
+        cols_max = [max(col) for col in columns]
+        increase = 0
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                increase += min(rows_max[i], cols_max[j]) - grid[i][j]
+        return increase
