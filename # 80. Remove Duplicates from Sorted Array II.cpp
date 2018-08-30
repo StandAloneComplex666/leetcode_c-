efd4acc9b71fa8ae9312d26869fc93622ff90c2b
Stand Alone Complex <yc2739@nyu.edu>
2018-08-30 17:47:04 -0400

    no message

---
+++ # 80. Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,20 @@
+public int removeDuplicates(int[] nums) {
+
+        // corner cases to add
+
+        int ptr = 1, i = 1, cnt = 0;
+        while (i < nums.length) {
+            if (nums[i] == nums[i - 1]) {
+                cnt++;
+                if (cnt >= 2) {
+                    i++;
+                    continue;
+                }
+            } else {
+                cnt = 0;
+            }
+            nums[ptr++] = nums[i++];
+        }
+
+        return ptr;
+    }
