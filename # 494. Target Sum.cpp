66550664c10c8c02fd29907d6d4ac136d310d44d
StandAloneComplex666 <yc2739@nyu.edu>
2018-12-10 11:01:07 +0800

    add a better method which reduce the redundancy in brute force method

--- # 494. Target Sum.cpp
+++ # 494. Target Sum.cpp
@@ -9,3 +9,19 @@ public:
         return find(p+1,nums,sum+nums[p])+find(p+1,nums,sum-nums[p]);
     }
 };
+
+//O(ns) Memoization. There is redundancy in brute force. A call to find() with the same start index and target sum can be made multiple times. 
+//We can use a 2d table to cache the result to avoid duplicate calls with the same state.
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        vector<unordered_map<int,int>> mem(nums.size());
+        return find(0,nums,S,mem);    
+    }
+    int find(int p, vector<int>& nums, int sum, vector<unordered_map<int,int>>& mem) {
+        if(p==nums.size()) return sum==0;
+        auto it = mem[p].find(sum);
+        if(it != mem[p].end()) return it->second;
+        return mem[p][sum]=find(p+1,nums,sum+nums[p],mem)+find(p+1,nums,sum-nums[p],mem);
+    }
+};
