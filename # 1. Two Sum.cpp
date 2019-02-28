c1438418116874055b56bf68fd68867cf5d3f39d
StandAloneComplex666 <yc2739@nyu.edu>
2019-02-28 16:42:13 +0800

    Update # 1. Two Sum.cpp

--- # 1. Two Sum.cpp
+++ # 1. Two Sum.cpp
@@ -6,6 +6,7 @@ vector<int> twoSum(vector<int> &numbers, int target)
 		vector<int> result;
 		for (int i = 0; i < numbers.size(); i++) {
 			int numberToFind = target - numbers[i];
+			
 	            //if numberToFind is found in map, return them
 			if (hash.find(numberToFind) != hash.end()) {
 	                    //+1 because indices are NOT zero based
