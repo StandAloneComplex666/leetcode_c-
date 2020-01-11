8456a591d5e09d4f285ce71f0a9cc361f6c1543d
StandAloneComplex666 <yc2739@nyu.edu>
2020-01-12 00:08:01 +0800

    Update # 355. Design Twitter.cpp

--- # 355. Design Twitter.cpp
+++ # 355. Design Twitter.cpp
@@ -59,6 +59,7 @@ public:
             following[followerID].erase(following[followerID].find(followeeID));
     }
 };
+
 /**
  * Your Twitter object will be instantiated and called as such:
  * Twitter obj = new Twitter();
